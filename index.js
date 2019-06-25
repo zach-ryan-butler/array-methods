a4c9268f7f0c562cdef8d66802c96d0e530feadd
Zach Butler <zach.standley93@gmail.com>
2019-06-25 09:00:46 -0700

    finished map function

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-
-
 const map = function map(arr, callback) {
     const array = [];
     for(let i = 0; i < arr.length; i++) {
         array[i] = callback(arr[i]);
     }
     return array;
-}
+};
 
 module.exports = { map };
